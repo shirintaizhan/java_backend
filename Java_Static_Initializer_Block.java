2ea962900279cb9be5c48ece50558fd2fcd98e9d
shirintaizhan <55387963+shirintaizhan@users.noreply.github.com>
2019-09-30 16:52:49 +0600

    Add files via upload

---
+++ Java_Static_Initializer_Block.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Java_Static_Initializer_Block {
+
+        static Scanner scan = new Scanner(System.in);
+        static int H = scan.nextInt();
+        static int B = scan.nextInt();
+        static boolean flag = true;
+        static{
+            if(H <= 0 || B <= 0){
+                flag = false;
+                System.out.println("java.lang.Exception: Breadth and height must be positive");
+            }
+        }
+
+        public static void main(String[] args){
+            if (flag) {
+                int area = B * H;
+                System.out.print(area);
+            }
+        }
+
+    }
+
---
+++ java_loops.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class java_loops {
+    public static void main(String[]  args){
+
+        System.out.print("input number:  ");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int result;
+        if (2 <= n && n <= 20) {
+            for (int i = 1; i <= 10; i++) {
+                result = n * i;
+                System.out.printf("\n %d * %d = %d", n, i, result);
+            }
+        }
+    }
+}
